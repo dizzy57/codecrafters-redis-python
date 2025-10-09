cfa3afbb226a7424c78cb294a565c4aa85dcca0a
Alexey Preobrazhenskiy <dizzy57@gmail.com>
2025-10-09 22:33:38 +0200

    Bind to a port

--- app/main.py
+++ app/main.py
@@ -1,15 +1,27 @@
-import socket  # noqa: F401
+import asyncio
+import logging
+import sys
+from typing import Final
+
+logger: Final = logging.getLogger(__name__)
 
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+async def handler(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
+    pass
 
-    # Uncomment this to pass the first stage
-    #
-    # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # server_socket.accept() # wait for client
+
+async def main() -> None:
+    logger.info("Starting server")
+    server = await asyncio.start_server(handler, "localhost", 6379)
+    async with server:
+        await server.serve_forever()
 
 
 if __name__ == "__main__":
-    main()
+    logging.basicConfig(
+        format="%(levelname).1s%(asctime)s.%(msecs)03d %(process)5d %(filename)s:%(lineno)d] %(message)s",
+        datefmt="%m%d %H:%M:%S",
+        level="INFO",
+        stream=sys.stdout,
+    )
+    asyncio.run(main())
