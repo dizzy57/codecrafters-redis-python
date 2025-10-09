e4bdde6bd180dbda2bff5a6b9f430efe21c9114b
Alexey Preobrazhenskiy <dizzy57@gmail.com>
2025-10-10 01:07:59 +0200

    Handle concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 import asyncio
+import contextvars
+import copy
 import logging
 import sys
 from typing import Final
 
 logger: Final = logging.getLogger(__name__)
+ctx_client: Final = contextvars.ContextVar("client", default="SERVER")
 
 
 async def handler(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
+    ctx_client.set(f"client={writer.get_extra_info("peername")}")
     logger.info("Client connected")
     while True:
         buf = await reader.read(1024)
@@ -25,11 +29,20 @@ async def main() -> None:
         await server.serve_forever()
 
 
+def filter_add_context(record: logging.LogRecord) -> logging.LogRecord:
+    record = copy.copy(record)
+    record.client = ctx_client.get()
+    return record
+
+
 if __name__ == "__main__":
     logging.basicConfig(
-        format="%(levelname).1s%(asctime)s.%(msecs)03d %(process)5d %(filename)s:%(lineno)d] %(message)s",
+        format="%(levelname).1s%(asctime)s.%(msecs)03d %(process)5d %(filename)s:%(lineno)d] %(client)s %(message)s",
         datefmt="%m%d %H:%M:%S",
         level="INFO",
         stream=sys.stdout,
     )
+    for h in logging.getLogger().handlers:
+        h.addFilter(filter_add_context)
+
     asyncio.run(main())
